ef2a2b855381e8d1e26197db4c641f24b80d9bb7
csharpmy-nunez <frankinnunez@gmail.com>
2018-03-30 22:12:05 -0700

    second commit

--- BotFrameworkSample/Dialogs/RootDialog.cs
+++ BotFrameworkSample/Dialogs/RootDialog.cs
@@ -2,6 +2,9 @@
 using System.Threading.Tasks;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Connector;
+using NetSpell;
+using System.IO;
+using System.Web.UI.WebControls;
 
 namespace BotFrameworkSample.Dialogs
 {
@@ -18,12 +21,44 @@ namespace BotFrameworkSample.Dialogs
 		private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
 		{
 			var activity = await result as Activity;
+			NetSpell.SpellChecker.Dictionary.WordDictionary oDict = new NetSpell.SpellChecker.Dictionary.WordDictionary();
+			string wordToCheck = activity.Text;
 
-			// calculate something for us to return
-			int length = (activity.Text ?? string.Empty).Length;
+			try
+			{
 
-			// return our reply to the user
-			await context.PostAsync($"You sent {activity.Text} which was {length} characters");
+				
+				oDict.DictionaryFile = "C:\\Program Files (x86)\\IIS Express\\en-US.dic";
+				oDict.Initialize();
+				NetSpell.SpellChecker.Spelling oSpell = new NetSpell.SpellChecker.Spelling();
+				oSpell.Dictionary = oDict;
+
+			
+				
+				// calculate something for us to return
+				int length = (activity.Text ?? string.Empty).Length;
+
+				// return our reply to the user
+				if (oSpell.TestWord(wordToCheck))
+				{
+					await context.PostAsync($"Word exists in the English dictionary");
+				}
+				else
+				{
+					await context.PostAsync($"Word does not exist in the English dictionary");
+				}
+			}
+			catch (FileNotFoundException fnfe)
+			{
+				await context.PostAsync($"File Was Not Found | Details: {fnfe.Message}");
+			}
+			finally
+			{
+				oDict.Dispose();
+			}
+
+	
+			//await context.PostAsync($"You sent {activity.Text} which was {length} characters From {activity.From}");
 
 			context.Wait(MessageReceivedAsync);
 		}
